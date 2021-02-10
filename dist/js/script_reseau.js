18cfaf1bbf6a0bb0c29b4a366ca58c72f9b46dd8
Clement  Iselin <clement-ise@MacBook-Pro-de-Clement.local>
2021-02-10 12:05:09 +0100

    CRUD file end

---
+++ dist/js/script_reseau.js
@@ -0,0 +1,25 @@
+/* Image et preview (createReseau)*/
+window.onload = function () {
+  function previewImage(event) {
+    // Reference à l'origine de l'évenement
+    var input = event.target
+
+    // Récupération d'un ou de champs file existants
+    // et renseignés
+    if (input.files && input.files[0]) {
+      // Création d'un objet FileReader
+      // fichier en lecture
+      var reader = new FileReader()
+
+      // Création d'un callback sur event onload
+      reader.onload = (e) => {
+        // Lecture de l'image en base64
+        // pour la charger dans imageData
+        $("#previewImage").attr("src", e.target.result)
+      }
+      // Render de l'image en tant qu'URL
+      // Format base64 pour affichage
+      reader.readAsDataURL(input.files[0])
+    }
+  }
+}
